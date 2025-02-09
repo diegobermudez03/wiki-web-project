a81af5fc62316147bf7b1265ca056b655a6f4ddf
Diego Bermudez <bermudezjimenezjuandiego@gmail.com>
2025-02-09 15:02:18 -0500

    first

---
+++ src/test/java/com/web_dev/wiki_project/WikiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_dev.wiki_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
